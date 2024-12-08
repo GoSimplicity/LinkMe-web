b1a6bc41cad3fda50e15fb7455f5f3d2f8e79e74
Bamboo <13664854532@163.com>
2024-12-08 21:23:31 +0800

    对接后端

--- linkme-admin/apps/web-antd/src/views/_core/authentication/register.vue
+++ linkme-admin/apps/web-antd/src/views/_core/authentication/register.vue
@@ -2,17 +2,35 @@
 import type { LoginAndRegisterParams } from '@vben/common-ui';
 
 import { ref } from 'vue';
+import { useRouter } from 'vue-router';
 
 import { AuthenticationRegister } from '@vben/common-ui';
 import { LOGIN_PATH } from '@vben/constants';
+import { registerApi } from '#/api/core/user';
+import { message } from 'ant-design-vue';
 
 defineOptions({ name: 'Register' });
 
+const router = useRouter();
 const loading = ref(false);
 
-function handleSubmit(value: LoginAndRegisterParams) {
-  // eslint-disable-next-line no-console
-  console.log('register submit:', value);
+async function handleSubmit(params: LoginAndRegisterParams) {
+  try {
+    loading.value = true;
+    await registerApi({
+      confirmPassword: params.confirmPassword,
+      password: params.password,
+      username: params.username,
+    });
+    // 注册成功
+    message.success('注册成功');
+    // 注册成功后跳转到登录页
+    router.push(LOGIN_PATH);
+  } catch (error) {
+    console.error('注册失败:', error);
+  } finally {
+    loading.value = false;
+  }
 }
 </script>
 
